3b6a9a8bc4a493ac3a3cc8ce9db24264e0a5afe1
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-01-17 22:05:42 -0800

    Moved Logging Directory
    For my use case, I deleted the Bugisoft.DMC directory every time I restarted the program in order to update. This allows for the log files to not be deleted in addition.

--- passives/Log.py
+++ passives/Log.py
@@ -4,9 +4,9 @@ from datetime import datetime
 class Log:
 
     def __init__(self):
-        self.filename = ("log-%s" % datetime.now().isoformat())
+        self.filename = ("home/Bugisoft/log-%s" % datetime.now().isoformat())
 
     async def addline(self, message):
         file = open(self.filename, 'a')
-        file.write(str(message))
+        file.write(str(message.content))
         file.close()
